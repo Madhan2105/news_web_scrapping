71c7dee5e83629b8328eb7271e9bd1681faef4c6
Madhan Nadar <madhanadar@outlook.com>
2020-06-14 13:38:53 +0530

    headless in globe

--- globenewswire.py
+++ globenewswire.py
@@ -19,7 +19,7 @@ def scrap_globenewswire(us_curr_time,temp_minute,logger):
         print("temp_minute",temp_minute)
         options  = webdriver.ChromeOptions()        
         options.add_argument("--start-maximized")
-        # options.add_argument('-headless')
+        options.add_argument('-headless')
         options.add_argument("--log-level=3")
         driver = webdriver.Chrome(options=options)
         wait = WebDriverWait(driver, 20)
